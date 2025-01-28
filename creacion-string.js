cfe235d9a52e608b84d77b03fa2bc45cd240e7fb
Ana Rotela <anarotela18@gmail.com>
2025-01-28 10:06:48 -0300

    Clase creación de string

---
+++ creacion-string.js
@@ -0,0 +1,18 @@
+// Tipo de dato: string
+let string1 = 'Hola, mundo';
+
+let string2 = "JavaScript es genial";
+
+let string3 = `${string1} feliz :)`;
+
+let string4 = string1 + ' '+ string2;
+
+let frase = 'JavaScript es Extredamente Genial';
+// Mostrar la longitud
+console.log(frase.length);
+// Mostrar en minúsucla
+console.log(frase.toLowerCase());
+// Mostrar en mayúsucla
+console.log(frase.toUpperCase());
+// Sub string consiste en el (index) y el (hasta donde va a ir + 1)
+console.log(frase.substring(0,10));
