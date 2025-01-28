d5c798188fa4386640bce3a404affe8d877ce9bb
Ana Rotela <anarotela18@gmail.com>
2025-01-28 10:58:00 -0300

    Clase conversión de tipos

---
+++ conversion-tipos.js
@@ -0,0 +1,15 @@
+/* Conversión de tipos: Type Casting y Coerción
+Lenguajes de programación:
+1- Compilados: C,C++,Rust,Go,Swift(Chequeo estático de tipos)
+2- Interpretados: JavaScript, Python, Ruby, PHP (Chequeo dinámico de tipos)
+*/
+
+const numero = 2;
+const booleano = true;
+console.log(numero + booleano); // 3
+
+/*
+Conversión
+Implícita 2 + true
+Explícita String() Number() Boolean() 
+*/
