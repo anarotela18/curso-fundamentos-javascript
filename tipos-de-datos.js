3a88a2644302faa9f38ce75c799733066577d736
Ana Rotela <anarotela18@gmail.com>
2025-01-28 09:57:12 -0300

    Clase Tipos de datos

---
+++ tipos-de-datos.js
@@ -0,0 +1,37 @@
+// TIPOS DE DATOS PRIMITIVOS
+// string
+let nombre = 'Maria';
+
+// number
+let edad = 18;
+
+// boolean
+let esMayorDeEdad = true;
+
+// null
+let noHayValor = null;
+
+// undefined
+let noDefinido = undefined;
+
+// symbol: indica un valor único
+let simboloUnico = Symbol("único");
+
+// bigint
+let numberoGrande = 2n;
+
+// TIPOS DE DATOS COMPLEJOS
+
+// object
+let carro = {
+    marca: 'Tesla',
+    modelo: 'Model S'
+};
+
+// array
+let frutas = ['manzana','banana','uvas'];
+
+// funciones
+function saludar(){
+    console.log("Hola Platzi!");
+}
