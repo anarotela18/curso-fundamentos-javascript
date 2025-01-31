f712f8da84ff57efedead2f5dc24e3d52d1a40af
Ana Rotela <anarotela18@gmail.com>
2025-01-31 11:40:13 -0300

    Clase loop for in

---
+++ loop-for-of.js
@@ -0,0 +1,9 @@
+/*
+for of: se usan en objetos iterables como arrays,strings porque terminan en una lista iterable
+*/
+
+let canasta = ["manzana","pera","naranja","uvas"];
+
+for(fruta of canasta){
+    console.log(fruta);
+}
