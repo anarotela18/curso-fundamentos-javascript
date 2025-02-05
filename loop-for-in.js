cf70d8f7b659a6d0367457c142529f2e9f8b91f3
Ana Rotela <anarotela18@gmail.com>
2025-02-05 09:46:19 -0300

    Clase loop for in

---
+++ loop-for-in.js
@@ -0,0 +1,17 @@
+/*
+for in: para objetos enumerables ejemplo Objetos
+*/
+const listaDeCompras = {
+    manzana: 5,
+    pera: 3,
+    naranja: 2,
+    uva: 1
+}
+
+for(fruta in listaDeCompras){
+    console.log(fruta);
+}
+
+for(fruta in listaDeCompras){
+    console.log(`${fruta} : ${listaDeCompras[fruta]}`);
+}
