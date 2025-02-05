839580ce2f87987f6a176471adbdb8b949f9125a
Ana Rotela <anarotela18@gmail.com>
2025-02-05 13:31:47 -0300

    Clase closure

---
+++ closure.js
@@ -0,0 +1,47 @@
+/*
+Un closure es cuando podemos acceder a aquellas variables que esten
+externas a nuestra funcion
+*/
+
+function outerFunction(){
+    let outerVariable = "I am from outer function";
+    
+    function innterFunction(){
+        console.log(outerVariable);
+    }
+    return innterFunction;
+}
+const closureExample = outerFunction();
+closureExample();
+
+function createCounter (){
+    let count = 0;
+    
+    return function(){
+        count++;
+        console.log(count);
+    }
+}
+
+const counterA = createCounter();
+counterA();
+counterA();
+
+const counterB = createCounter();
+counterB();
+
+
+function outer(){
+    let message = 'Hello, ';
+
+    function inner(name){
+        console.log(message + name);
+    }
+
+    return inner;
+}
+const closureA = outer();
+const closureB = outer();
+
+closureA('Alicia');
+closureB('Bob');
